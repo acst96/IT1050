5f7ca1eb9ec02776e13a05ac94cd647b94604a63
Aaron Scott <S01125467@acad.tri-c.edu>
2019-09-24 20:48:23 -0400

    Here are some changes.

---
+++ Assignment1/Assignment1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.) compute Full name 
+            //2.) compute height cm (double)
+            //3.) compute if you can vote (bool)
+
+
+            string firstname;
+
+            string middlename;
+            string lastname;
+
+            Console.WriteLine("fristname:");
+
+            firstname = Console.ReadLine();
+
+            Console.WriteLine("middlename:");
+
+            middlename = Console.ReadLine();
+
+            Console.WriteLine("lastname:");
+            lastname = Console.ReadLine();
+
+
+            string fullname = firstname + " " + middlename + " " + lastname;
+
+
+
+            firstname = System.Console.ReadLine();
+
+            Console.WriteLine(fullname);
+
+            Console.ReadKey();
+            //////////////////////////////////////////////////
+            //////////////////////////////////////////////////
+
+            const double cm_multiplier = 2.54;
+            const double inches_per_foot = 12;
+            int heightfeet;
+            double heightinches;
+
+            Console.WriteLine("Height Feet: ");
+            heightfeet = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Height Inches:");
+            heightinches = int.Parse(Console.ReadLine());
+
+            double inches = heightfeet * inches_per_foot;
+
+            inches += heightinches;
+
+            double heightcm = cm_multiplier = inches;
+
+            Console.WriteLine();
+            Console.WriteLine(heightcm);
+
+            Console.WriteLine();
+            Console.ReadKey();
+
+            //////////////////////////////////
+            ///
+            int age;
+            bool iscitizen;
+
+            Console.Write("YourAge");
+            age = int.Parse(Console.ReadLine());
+
+            Console.Write("citizen?");
+            bool iscitizen = Console.ReadLine().ToLower()startwith("y");
+        }
+    }
+}
