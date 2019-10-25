2974aaf2337bb6dd1c6daa7d0c5ceb7d7246b37c
acst96 <aaronscott96@me.com>
2019-10-24 21:16:38 -0400

    new code

---
+++ Asignment 2/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asignment_2
+{
+    class Person
+    {
+        public Person
+        public string Firstname;
+        public string Lastname;
+        public int Age;
+
+     
+
+       
+
+
+        public static double SumofAllAges;
+     
+        public string GetFullName()
+        {
+            return this.Firstname + " " + this.Lastname;
+        }
+
+        
+         
+        
+
+
+    }
+}
---
+++ Asignment 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Asignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            Person p2 = new Person();
+            Console.WriteLine("whats your First Name?");
+            p1.Firstname = Console.ReadLine();
+            Console.WriteLine("whats your Last Name?");
+            p1.Lastname = Console.ReadLine();
+            Console.WriteLine("Whats your Age?");
+            p1.Age = int.Parse(Console.ReadLine());
+            p2 = new Person();
+            Console.WriteLine("Whats your First Name?");
+            p2.Firstname = Console.ReadLine();
+            Console.WriteLine("Whats your Last Name?");
+            p2.Lastname = Console.ReadLine();
+             
+            
+
+        
+
+        }
+
+
+}
+
+
+}
